890021e8cf97e41cba6e6ab463fbfbbf99f0fc47
jayathracm <jayathracm@gmail.com>
2024-12-05 03:52:03 +0200

    added render backend url

--- src/api/index.js
+++ src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:3001", 
+  baseURL: "https://reelradarbackend.onrender.com", 
 });
 
 API.interceptors.request.use(
